f18b31067cf191f90667fe7225cdcabbf5940ab4
Noro190695 <norojan56@mail.ru>
2021-07-26 21:19:51 +0400

    commit

---
+++ assets/script/index.js
@@ -0,0 +1,21 @@
+const socket = io();
+const message = document.querySelector('.message');
+const form = document.querySelector('form');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let [data] = new FormData(e.target)
+    data = data[1];
+    if(!data) return;
+    socket.emit('chat message', {
+        message: data
+    });
+    e.target.children[0].value = ''
+})
+
+socket.on('chat message', data => {
+    let li = document.createElement('li');
+    li.textContent = data.message;
+    message.appendChild(li)
+})
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const PORT = 5000;
+
+
+app.get('/', (res,req) => {
+    req.sendFile(path.resolve(__dirname, 'index.html'))
+});
+io.on('connection', socket => {
+    socket.on('chat message', data => {
+        io.emit('chat message', {
+            message: data.message
+        })
+    })
+})
+app.use(express.static(path.resolve(__dirname, 'assets')))
+http.listen(5000, () => {
+    console.log('Server Started');
+})
